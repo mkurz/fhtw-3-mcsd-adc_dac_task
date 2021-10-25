4a9c40fc663707e8aa28cd7cc5cb11120da08361
Matthias Kurz <m.kurz@irregular.at>
2021-10-25 13:02:42 +0200

    Fire every 20ms

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -299,7 +299,7 @@ static void MX_TIM6_Init(void)
   htim6.Instance = TIM6;
   htim6.Init.Prescaler = 32000-1;
   htim6.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim6.Init.Period = 49;
+  htim6.Init.Period = 20-1;
   htim6.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
   if (HAL_TIM_Base_Init(&htim6) != HAL_OK)
   {
